09da8402ab6d499dc968566a78ad5aa7655dfc75
Raydar <37029157+raydar32@users.noreply.github.com>
2020-11-20 17:56:49 +0100

    Update
    Obtained some serious speedup by changing data type to float and the AtomicAdd operation

--- cuda_k_means/cuda_k_means/kernel.cu
+++ cuda_k_means/cuda_k_means/kernel.cu
@@ -20,27 +20,11 @@
 
 #define COORD_MAX 100000
 #define CLUSTER_NUM 20
-#define POINT_NUM 10000
+#define POINT_NUM 10000000
 #define POINT_FEATURES 3
 #define CLUSTER_FEATURES 4
 #define THREAD_PER_BLOCK 1024
-#if !defined(__CUDA_ARCH__) || __CUDA_ARCH__ >= 600
 
-#else
-static __inline__ __device__ double atomicAdd(double* address, double val) {
-	unsigned long long int* address_as_ull = (unsigned long long int*)address;
-	unsigned long long int old = *address_as_ull, assumed;
-	if (val == 0.0)
-		return __longlong_as_double(old);
-	do {
-		assumed = old;
-		old = atomicCAS(address_as_ull, assumed, __double_as_longlong(val + __longlong_as_double(assumed)));
-	} while (assumed != old);
-	return __longlong_as_double(old);
-}
-
-
-#endif
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort = true)
 {
@@ -52,38 +36,38 @@ inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort =
 }
 
 
-double random_double() {
-	double x;
-	x = (double)rand() * (double)32767;
+float random_float() {
+	float x;
+	x = (float)rand() * (float)32767;
 	x = fmod(x, COORD_MAX);
 	return x;
 }
 
-void print_points(double* punti) {
+void print_points(float* punti) {
 	printf("----------- Punti -------------\n");
 	for (int i = 0; i < POINT_NUM; i++) {
-		double x = punti[i * POINT_FEATURES + 0];
-		double y = punti[i * POINT_FEATURES + 1];
-		double cluster = punti[i * POINT_FEATURES + 2];
+		float x = punti[i * POINT_FEATURES + 0];
+		float y = punti[i * POINT_FEATURES + 1];
+		float cluster = punti[i * POINT_FEATURES + 2];
 		printf("punto %d, x:%f y:%f, c:%f\n", i, x, y, cluster);
 	}
 	printf("-------------------------------\n");
 }
-void print_clusters(double* clusters) {
+void print_clusters(float* clusters) {
 	printf("----------- Cluster -------------\n");
 	for (int i = 0; i < CLUSTER_NUM; i++) {
-		double centro = clusters[i * CLUSTER_FEATURES + 0];
-		double sizex = clusters[i * CLUSTER_FEATURES + 1];
-		double sizey = clusters[i * CLUSTER_FEATURES + 2];
-		double numPoints = clusters[i * CLUSTER_FEATURES + 3];
+		float centro = clusters[i * CLUSTER_FEATURES + 0];
+		float sizex = clusters[i * CLUSTER_FEATURES + 1];
+		float sizey = clusters[i * CLUSTER_FEATURES + 2];
+		float numPoints = clusters[i * CLUSTER_FEATURES + 3];
 		printf("cluster %d, centro: %f, sizex; %f, sizey:%f, numpoints: %f\n", i, centro, sizex, sizey, numPoints);
 	}
 	printf("-------------------------------\n");
 }
-void init_all(double* punti, double* clusters) {
+void init_all(float* punti, float* clusters) {
 	for (int i = 0; i < POINT_NUM; i++) {			//punto: <x,y,cluster>
-		punti[i * POINT_FEATURES + 0] = random_double();
-		punti[i * POINT_FEATURES + 1] = random_double();
+		punti[i * POINT_FEATURES + 0] = random_float();
+		punti[i * POINT_FEATURES + 1] = random_float();
 		punti[i * POINT_FEATURES + 2] = 0;
 	}
 
@@ -94,30 +78,30 @@ void init_all(double* punti, double* clusters) {
 		clusters[i * CLUSTER_FEATURES + 3] = 0;
 	}
 }
-void write_to_file(double* punti) {
+void write_to_file(float* punti) {
 	printf("\nStampo su file!\n");
 	FILE* fPtr;
 	char filePath[100] = { "G:\\file.dat" };
 	char dataToAppend[1000];
 	fPtr = fopen(filePath, "a");
 	for (int i = 0; i < POINT_NUM; i++) {
-		double x = punti[i * POINT_FEATURES + 0];
-		double y = punti[i * POINT_FEATURES + 1];
+		float x = punti[i * POINT_FEATURES + 0];
+		float y = punti[i * POINT_FEATURES + 1];
 		int cluster = punti[i * POINT_FEATURES + 2];
 		fprintf(fPtr, "%f %f %d\n", x, y, cluster);
 	}
 }
-__device__ double distance(double x1, double x2, double y1, double y2) {
+__device__ float distance(float x1, float x2, float y1, float y2) {
 	return sqrt(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)));
 }
 
 
 
-__global__ void assign_clusters(double* punti, double* clusters) {
+__global__ void assign_clusters(float* punti, float* clusters) {
 
 	long id_punto = threadIdx.x + blockIdx.x * blockDim.x;
 	if (id_punto < POINT_NUM) {
-		double x_punto, x_cluster, y_punto, y_cluster = 0;
+		float x_punto, x_cluster, y_punto, y_cluster = 0;
 		x_punto = punti[id_punto * POINT_FEATURES + 0];
 		y_punto = punti[id_punto * POINT_FEATURES + 1];
 		long best_fit = 0;
@@ -138,26 +122,26 @@ __global__ void assign_clusters(double* punti, double* clusters) {
 		//clusters[best_fit * CLUSTER_FEATURES + 1] = clusters[best_fit * CLUSTER_FEATURES + 1] + x_punto;
 		//clusters[best_fit * CLUSTER_FEATURES + 2] = clusters[best_fit * CLUSTER_FEATURES + 2] + y_punto;
 		//clusters[best_fit * CLUSTER_FEATURES + 3] = clusters[best_fit * CLUSTER_FEATURES + 3] + 1;
-		atomicAdd(&clusters[best_fit * CLUSTER_FEATURES + 3],1);
+		atomicAdd(&clusters[best_fit * CLUSTER_FEATURES + 3], 1);
 
 		//printf("id %d: Assegno il punto %f,%f al cluster %f\n",id_punto, x_punto, y_punto, punti[id_punto * POINT_FEATURES + 2]);
 	}
 
 }
 
-__global__ void cluster_recomputecenters_cuda(double* points, double* clusters) {
+__global__ void cluster_recomputecenters_cuda(float* points, float* clusters) {
 
 	bool flag;
 	long id_cluster = threadIdx.x + blockIdx.x * blockDim.x;
 	//printf("Ricalcolo il cluster %d\n", id_cluster);
-	double sizeX = clusters[id_cluster * CLUSTER_FEATURES + 1];
-	double sizeY = clusters[id_cluster * CLUSTER_FEATURES + 2];
-	double nPoints = clusters[id_cluster * CLUSTER_FEATURES + 3];
-	double newX = sizeX / nPoints;
-	double newY = sizeY / nPoints;
+	float sizeX = clusters[id_cluster * CLUSTER_FEATURES + 1];
+	float sizeY = clusters[id_cluster * CLUSTER_FEATURES + 2];
+	float nPoints = clusters[id_cluster * CLUSTER_FEATURES + 3];
+	float newX = sizeX / nPoints;
+	float newY = sizeY / nPoints;
 	long cluster_center_index = (long)clusters[id_cluster * CLUSTER_FEATURES + 0];
-	double x = points[cluster_center_index * POINT_FEATURES + 0];
-	double y = points[cluster_center_index * POINT_FEATURES + 1];
+	float x = points[cluster_center_index * POINT_FEATURES + 0];
+	float y = points[cluster_center_index * POINT_FEATURES + 1];
 
 	if (x == newX && y == newY) {
 		flag = false;
@@ -173,7 +157,7 @@ __global__ void cluster_recomputecenters_cuda(double* points, double* clusters) 
 
 }
 
-__global__ void cuda_remove_points_cluster(double *clusters) {
+__global__ void cuda_remove_points_cluster(float* clusters) {
 	//<centro, size_x, size_y, punti>
 	long id_cluster = threadIdx.x + blockIdx.x * blockDim.x;
 	clusters[id_cluster * CLUSTER_FEATURES + 1] = 0;
@@ -183,22 +167,22 @@ __global__ void cuda_remove_points_cluster(double *clusters) {
 
 int main()
 {
-	double* punti = (double*)malloc(POINT_NUM * POINT_FEATURES * sizeof(double));
-	double* clusters = (double*)malloc(CLUSTER_NUM * CLUSTER_FEATURES * sizeof(double));
+	float* punti = (float*)malloc(POINT_NUM * POINT_FEATURES * sizeof(float));
+	float* clusters = (float*)malloc(CLUSTER_NUM * CLUSTER_FEATURES * sizeof(float));
 
 	init_all(punti, clusters);
 
-	double* punti_d = 0;
-	double* cluster_d = 0;
+	float* punti_d = 0;
+	float* cluster_d = 0;
 
 	int iterazioni = 10;
 	clock_t begin = clock();
 
-	cudaMalloc(&punti_d, POINT_NUM * POINT_FEATURES * sizeof(double));
-	cudaMalloc(&cluster_d, CLUSTER_NUM * CLUSTER_FEATURES * sizeof(double));
+	cudaMalloc(&punti_d, POINT_NUM * POINT_FEATURES * sizeof(float));
+	cudaMalloc(&cluster_d, CLUSTER_NUM * CLUSTER_FEATURES * sizeof(float));
 
-	cudaMemcpy(punti_d, punti, POINT_NUM * POINT_FEATURES * sizeof(double), cudaMemcpyHostToDevice);
-	cudaMemcpy(cluster_d, clusters, CLUSTER_NUM * CLUSTER_FEATURES * sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(punti_d, punti, POINT_NUM * POINT_FEATURES * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(cluster_d, clusters, CLUSTER_NUM * CLUSTER_FEATURES * sizeof(float), cudaMemcpyHostToDevice);
 
 	for (int i = 0; i < iterazioni; i++) {
 
@@ -207,14 +191,14 @@ int main()
 		cudaDeviceSynchronize();
 		cluster_recomputecenters_cuda << <1, CLUSTER_NUM >> > (punti_d, cluster_d);
 		cudaDeviceSynchronize();
-		cuda_remove_points_cluster << <1, CLUSTER_NUM>>>(cluster_d);
+		cuda_remove_points_cluster << <1, CLUSTER_NUM >> > (cluster_d);
 		cudaDeviceSynchronize();
 		//clusters_recomputeCenter(punti, clusters);
 	}
-	cudaMemcpy(punti, punti_d, POINT_NUM * POINT_FEATURES * sizeof(double), cudaMemcpyDeviceToHost);
-	cudaMemcpy(clusters, cluster_d, CLUSTER_NUM * CLUSTER_FEATURES * sizeof(double), cudaMemcpyDeviceToHost);
+	cudaMemcpy(punti, punti_d, POINT_NUM * POINT_FEATURES * sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(clusters, cluster_d, CLUSTER_NUM * CLUSTER_FEATURES * sizeof(float), cudaMemcpyDeviceToHost);
 	clock_t end = clock();
-	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	float time_spent = (float)(end - begin) / CLOCKS_PER_SEC;
 	printf("Tempo %f", time_spent);
 	//print_points(punti);
 	//print_clusters(clusters);
